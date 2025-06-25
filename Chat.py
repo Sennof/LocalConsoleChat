077035fc5656639be9d6a2b264a9e60e8eb7ea75
sennof <arseniynoskin@gmail.com>
2025-06-25 16:14:26 +0300

    feat: message formatting & threading

--- Chat.py
+++ Chat.py
@@ -1,8 +1,9 @@
 import threading
+import datetime
 
 def send_messages(conn):
     while True:
-        dataSend = input("[->] ").encode()
+        dataSend = input("").encode()
         conn.send(len(dataSend).to_bytes(4, "big"))
         conn.send(dataSend)
 
@@ -10,7 +11,7 @@ def get_messages(conn):
     while True:
         message_size = int.from_bytes(conn.recv(4), "big")
         dataGot: str = conn.recv(message_size).decode()
-        print(f"[<-] {dataGot}")
+        print(f"[{datetime.datetime.now}] {dataGot}")
 
 def start_chat(conn):
     threading.Thread(target=send_messages, args=(conn,), daemon=True).start()
@@ -18,6 +19,8 @@ def start_chat(conn):
     while True:
         try:
             conn.send(b'')
+        except KeyboardInterrupt:
+            exit()
         except:
             print("Connection lost")
             break
