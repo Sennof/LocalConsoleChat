dd03d0ed49f00dba34943189756bb8cfcf64c997
Scaik <evseikazansky2008@gmail.com>
2025-06-25 16:19:40 +0300

    fix: display time on message receive

--- Chat.py
+++ Chat.py
@@ -2,20 +2,26 @@ import threading
 import datetime
 
 def send_messages(conn):
-    while True:
+    t = threading.currentThread()
+    while getattr(t, "do_run", True):
         dataSend = input("").encode()
         conn.send(len(dataSend).to_bytes(4, "big"))
         conn.send(dataSend)
 
 def get_messages(conn):
-    while True:
+    t = threading.currentThread()
+    while getattr(t, "do_run", True):
         message_size = int.from_bytes(conn.recv(4), "big")
         dataGot: str = conn.recv(message_size).decode()
-        print(f"[{datetime.datetime.now}] {dataGot}")
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(f"[{current_time}] {dataGot}")
 
 def start_chat(conn):
-    threading.Thread(target=send_messages, args=(conn,), daemon=True).start()
-    threading.Thread(target=get_messages, args=(conn,), daemon=True).start()
+    t1 = threading.Thread(target=send_messages, args=(conn,), daemon=True)
+    t1.start()
+    t2 = threading.Thread(target=get_messages, args=(conn,), daemon=True)
+    t2.start()
+
     while True:
         try:
             conn.send(b'')
@@ -23,5 +29,6 @@ def start_chat(conn):
             exit()
         except:
             print("Connection lost")
+            t1.do_run = False
+            t2.do_run = False
             break
-
