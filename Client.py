fdafb8b84a1a4d884766a6be2166dd23003483af
Scaik <evseikazansky2008@gmail.com>
2025-06-25 15:45:30 +0300

    feat: client

---
+++ Client.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = "192.168.25.237"
+PORT = 8800
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((HOST, PORT))
+
+msg = "Hello, Server!".encode()
+s.send(len(msg).to_bytes(4, "big"))
+s.send(msg)
+
+msg_size = int.from_bytes(s.recv(4), "big")
+msg = s.recv(msg_size)
+
+print(msg.decode())
